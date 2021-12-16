4f7060916da62ff890dc222eaa0e087067a7d7a2
wilgur513 <wilgur513@naver.com>
2021-12-16 15:56:00 +0900

    feat: save initial stations

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,15 @@
 package subway;
 
+import java.util.Arrays;
+import subway.domain.Station;
+import subway.domain.StationRepository;
 import static subway.view.InputView.inputOption;
 import static subway.view.OutputView.*;
 
 public class Application {
 	public static void main(String[] args) {
+		saveStations();
+
 		printMainPage();
 		String option = inputOption();
 
@@ -15,5 +20,10 @@ public class Application {
 		} else if(option.equals("3")) {
 			printManageSectionPage();
 		}
+	}
+
+	private static void saveStations() {
+		String[] stationNames = {"교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"};
+		Arrays.stream(stationNames).map(Station::of).forEach(StationRepository::addStation);
 	}
 }
