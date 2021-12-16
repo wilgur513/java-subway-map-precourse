186bae33b6f20942d651f90e41b6789634653baf
wilgur513 <wilgur513@naver.com>
2021-12-16 16:13:48 +0900

    feat: add station

--- src/main/java/subway/controller/SubwayController.java
+++ src/main/java/subway/controller/SubwayController.java
@@ -1,24 +1,38 @@
 package subway.controller;
 
-import static subway.view.InputView.inputOption;
+import subway.domain.Station;
+import subway.domain.StationRepository;
+import static subway.view.InputView.*;
 import static subway.view.OutputView.*;
-import static subway.view.OutputView.printManageSectionPage;
 
 public class SubwayController {
 	public void service() {
-		while(true) {
+		while (true) {
 			printMainPage();
 			String option = inputOption();
 
 			if (option.equals("1")) {
-				printManageStationPage();
-			} else if(option.equals("2")) {
+				manageStation();
+			} else if (option.equals("2")) {
 				printManageLinePage();
-			} else if(option.equals("3")) {
+			} else if (option.equals("3")) {
 				printManageSectionPage();
-			} else if(option.equals("Q")) {
+			} else if (option.equals("Q")) {
 				break;
 			}
 		}
 	}
+
+	private void manageStation() {
+		printManageStationPage();
+		String option = inputOption();
+
+		if (option.equals("1")) {
+			String stationName = inputStation();
+			Station station = Station.of(stationName);
+			StationRepository.addStation(station);
+		}
+	}
+
+
 }
--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -6,9 +6,17 @@ public class InputView {
 	private static final Scanner SCANNER = new Scanner(System.in);
 
 	public static String inputOption() {
-		System.out.println("## 원하는 기능을 선택하세요.");
-		String option = SCANNER.next();
+		return inputWithMessage("원하는 기능을 선택하세요.");
+	}
+
+	public static String inputStation() {
+		return inputWithMessage("등록할 역 이름을 입력하세요.");
+	}
+
+	private static String inputWithMessage(String message) {
+		System.out.println("## " + message);
+		String value = SCANNER.next();
 		System.out.println();
-		return option;
+		return value;
 	}
 }
