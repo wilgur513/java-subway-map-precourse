b160b90157766f39ebc17f969f6da3ad5c15a0d0
wilgur513 <wilgur513@naver.com>
2021-12-16 15:46:48 +0900

    feat: print manage station page

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -2,10 +2,15 @@ package subway;
 
 import static subway.view.InputView.inputOption;
 import static subway.view.OutputView.printMainPage;
+import static subway.view.OutputView.printManageStationPage;
 
 public class Application {
-    public static void main(String[] args) {
-        printMainPage();
-        String option = inputOption();
-    }
+	public static void main(String[] args) {
+		printMainPage();
+		String option = inputOption();
+
+		if (option.equals("1")) {
+            printManageStationPage();
+		}
+	}
 }
--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -10,4 +10,13 @@ public class OutputView {
 		System.out.println("Q. 종료");
 		System.out.println();
 	}
+
+	public static void printManageStationPage() {
+		System.out.println("## 역 관리 화면");
+		System.out.println("1. 역 등록");
+		System.out.println("2. 역 삭제");
+		System.out.println("3. 역 조회");
+		System.out.println("B. 돌아가기");
+		System.out.println();
+	}
 }
