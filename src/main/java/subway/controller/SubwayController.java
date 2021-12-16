c31797ca80f5e4d9086c32735231f77014d63f02
wilgur513 <wilgur513@naver.com>
2021-12-16 16:08:42 +0900

    feat: quit application by input option

--- src/main/java/subway/controller/SubwayController.java
+++ src/main/java/subway/controller/SubwayController.java
@@ -6,15 +6,19 @@ import static subway.view.OutputView.printManageSectionPage;
 
 public class SubwayController {
 	public void service() {
-		printMainPage();
-		String option = inputOption();
+		while(true) {
+			printMainPage();
+			String option = inputOption();
 
-		if (option.equals("1")) {
-			printManageStationPage();
-		} else if(option.equals("2")) {
-			printManageLinePage();
-		} else if(option.equals("3")) {
-			printManageSectionPage();
+			if (option.equals("1")) {
+				printManageStationPage();
+			} else if(option.equals("2")) {
+				printManageLinePage();
+			} else if(option.equals("3")) {
+				printManageSectionPage();
+			} else if(option.equals("Q")) {
+				break;
+			}
 		}
 	}
 }
